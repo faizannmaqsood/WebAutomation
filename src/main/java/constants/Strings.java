f67c972a13b99a0e9f0f21abc45f6e63a1d5a245
Faizan.Maqsood <faizann.maqsood@gmail.com>
2024-06-01 00:30:22 +0500

    web 7

--- src/main/java/constants/Strings.java
+++ src/main/java/constants/Strings.java
@@ -5,7 +5,15 @@ public class Strings {
 
     public static String DashboardTitleString = "Homepage - Ultimate QA";
 
-    public static String EducationTextString = "Education";
+    public static String DashboardServiceString = "Services";
+
+    public static String DashboardAboutString = "About";
+
+    public static String DashboardBlogString = "Blog";
+
+    public static String DashboardDiscoveryString = "I want a free DISCOVERY SESSION";
+
+
 
 
 
--- src/test/java/UltimateQA/UltimateQATest.java
+++ src/test/java/UltimateQA/UltimateQATest.java
@@ -10,8 +10,13 @@ import utils.Listeners.TestListener;
 import utils.extentreports.ExtentTestManager;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
@@ -34,7 +39,7 @@ public class UltimateQATest {
 
 
         Playwright playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
         page = browser.newPage();
         page.setDefaultTimeout(120000);
         DashboardPage = new UltimateDashboardPage(page);
@@ -79,6 +84,8 @@ public class UltimateQATest {
         // Check if the element is clickable
         boolean isClickable = page.isEnabled("#menu-item-218392");
         System.out.println("Is element clickable: " + isClickable);
+        String ActualValue = page.textContent("#menu-item-218392");
+        Assert.assertEquals(Strings.DashboardServiceString, ActualValue);
 
         // If element is clickable, click on it
         if (isClickable) {
@@ -99,6 +106,9 @@ public class UltimateQATest {
         ExtentTestManager.startTest("testVerifyingAboutLink",
                 "Verifying the click on About link");
         // Check if the element is clickable
+
+        String ActualValue = page.textContent("#menu-item-217940");
+        Assert.assertEquals(Strings.DashboardAboutString, ActualValue);
         boolean isClickable = page.isEnabled("#menu-item-217940");
         System.out.println("Is element clickable: " + isClickable);
 
@@ -120,6 +130,9 @@ public class UltimateQATest {
 
         ExtentTestManager.startTest("testVerifyingBlogLink",
                 "Verifying the click on Blog link");
+
+        String ActualValue = page.textContent("#menu-item-218226");
+        Assert.assertEquals(Strings.DashboardBlogString, ActualValue);
         // Check if the element is clickable
         boolean isClickable = page.isEnabled("#menu-item-218226");
         System.out.println("Is element clickable: " + isClickable);
@@ -142,6 +155,8 @@ public class UltimateQATest {
 
         ExtentTestManager.startTest("testVerifyingEducationLink",
                 "Verifying the click on education link");
+
+
         // Check if the element is clickable
         boolean isClickable = page.isEnabled("#menu-item-218225");
         System.out.println("Is element clickable: " + isClickable);
@@ -156,14 +171,196 @@ public class UltimateQATest {
 
     }
 
+    @Test(priority = 6)
+    public void TestDiscoveryLink(){
+
+        ExtentTestManager.startTest("testVerifyingDiscoverSessionLink",
+                "Verifying the click on Discovery link");
+
+        String ActualValue = page.textContent("#menu-item-217945");
+        Assert.assertEquals(Strings.DashboardDiscoveryString, ActualValue);
+        // Check if the element is clickable
+        boolean isClickable = page.isEnabled("#menu-item-217945");
+        System.out.println("Is element clickable: " + isClickable);
+
+        // If element is clickable, click on it
+        if (isClickable) {
+            page.click("#menu-item-218225");
+            System.out.println("Clicked on the element");
+            page.evaluate("window.scrollBy(0, document.body.scrollHeight)");
+
+            try {
+                Thread.sleep(3000); // Wait for 3 seconds
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Element is not clickable");
+        }
+
+    }
+
+    @Test(priority = 7)
+    public void TestLogoImage() throws IOException {
+
+        ExtentTestManager.startTest("testVerifyingLogoImage",
+                "Verifying the image is available");
+
+        String IMAGE_PATH = "downloaded_image.svg";
+        Path imagePath = Paths.get(IMAGE_PATH);
+        if (Files.exists(imagePath)) {
+            Files.delete(imagePath);
+        }
+        Locator imgLocator = page.locator("img[data-src='https://ultimateqa.com/wp-content/uploads/2023/11/logo.svg']");
+
+
+
+        String imgSrc = imgLocator.getAttribute("src");
+        System.out.println("Image src attribute: " + imgSrc);
+
+
+        // Check if the image is present
+        boolean isImagePresent = imgLocator.count() > 0;
+        if (isImagePresent) {
+            System.out.println("Image is present on the page.");
+        } else {
+            System.out.println("Image is not present on the page.");
+        }
+
+        // Download the image
+        downloadImage(imgSrc, "downloaded_image.svg");
+
+
+    }
+
+
+    @Test(priority = 8)
+    public void TestHeroImage() throws IOException {
+
+        ExtentTestManager.startTest("testVerifyingHeroImage",
+                "Verifying the hero image is available");
+
+        page.click("img[data-src='https://ultimateqa.com/wp-content/uploads/2023/11/logo.svg']");
+        String IMAGE_PATH = "hero_image.svg";
+        Path imagePath = Paths.get(IMAGE_PATH);
+        if (Files.exists(imagePath)) {
+            Files.delete(imagePath);
+        }
+        Locator imgLocator = page.locator("img[title='hero-image']");
+
+
+
+        String imgSrc = imgLocator.getAttribute("src");
+        System.out.println("Image src attribute: " + imgSrc);
+
+
+        // Check if the image is present
+        boolean isImagePresent = imgLocator.count() > 0;
+        if (isImagePresent) {
+            System.out.println("Image is present on the page.");
+        } else {
+            System.out.println("Image is not present on the page.");
+        }
+
+        // Download the image
+        downloadImage(imgSrc, "hero_image.webp");
+
+
+    }
+
+@Test(priority = 9)
+    public void ScrollToMId(){
+
+    ExtentTestManager.startTest("testVerifyScrollToMid",
+            "Verifying the browser is");
+
+        scrollToBottom(page);
+
+
+        page.waitForTimeout(5000);
+    }
+
+    @Test(priority = 10)
+    public void ScheduleDiscoverySession(){
+
+        ExtentTestManager.startTest("testVerifyScheduleDiscoverySession",
+                "Verifying the Schedule of discovery session");
+
+        // Wait for the link to be available in the DOM
+        page.waitForSelector(".et_pb_button.et_pb_button_15");
+
+        // Click the link
+        page.click(".et_pb_button.et_pb_button_15");
+
+        page.waitForTimeout(20000);
+        // Get all open pages (tabs)
+        BrowserContext context = browser.contexts().get(0);
+        Page secondPage = context.pages().get(1);  // Index 1 for the second tab
+
+        // Switch to the second tab
+        browser.contexts().get(0).pages().get(1).bringToFront();
+        page.waitForTimeout(5000);
+
+        secondPage.fill("#cu-form-control-0", "Faizan");
+
+        secondPage.fill("#cu-form-control-1", "faizann.maqsood@gmail.com");
+
+        secondPage.fill("#cu-form-control-2", "Automation Engineer");
+
+        secondPage.fill("#cu-form-control-3", "Microfinance bank");
+
+        secondPage.click("[data-test='select__dropdown__toggle']");
+
+        // Wait for the dropdown element to be visible and enabled
+        secondPage.waitForSelector(".cu-custom-fields__dropdown-item .cu-custom-fields__dropdown-text");
+
+        // Click on the specific item within the dropdown
+        secondPage.click(".cu-custom-fields__dropdown-item .cu-custom-fields__dropdown-text");
+
+        secondPage.fill("#cu-form-control-5", "This is automation testing against ultimate QA website");
+
+        page.waitForTimeout(5000);
+
+
+
+      //  Assert.assertEquals(Strings.DashboardServiceString, elementContent);
+
+    }
+
+
+    public static void scrollToMiddle(Page page) {
+        page.evaluate("window.scrollTo(0, document.body.scrollHeight / 2 * 0.9);");
+    }
+
+    public static void scrollToBottom(Page page) {
+        page.evaluate("window.scrollTo(0, document.body.scrollHeight);");
+    }
+
+    public static void downloadImage(String urlString, String destinationFile) throws IOException {
+        URL url = new URL(urlString);
+        try (InputStream in = url.openStream()) {
+            Files.copy(in, Paths.get(destinationFile), StandardCopyOption.REPLACE_EXISTING);
+        }
+    }
+
+
     // Method to get HTTP response status code
-    private static int getResponseStatus(String url) {
+    public static int getResponseStatus(String url) {
+        final int TIMEOUT_MILLIS = 5000; // 5 seconds
+        HttpURLConnection connection = null;
         try {
-            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            connection = (HttpURLConnection) new URL(url).openConnection();
             connection.setRequestMethod("HEAD");
+            connection.setConnectTimeout(TIMEOUT_MILLIS);
+            connection.setReadTimeout(TIMEOUT_MILLIS);
             return connection.getResponseCode();
         } catch (Exception e) {
+            e.printStackTrace(); // Log the exception
             return -1; // Error occurred while checking status
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
         }
     }
 
